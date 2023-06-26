bf92a3f10377de7ccec488775a5f58c008b27000
mtyutyulnikov <mishatyty@conda activate eco2ai>
2023-06-26 11:21:31 +0300

    MNIST Tutorial

--- scheduler_vm_task/google_cloud_example.py
+++ scheduler_vm_task/google_cloud_example.py
@@ -1,10 +1,11 @@
 from google_cloud_vm_moving import google_cloud_move_vm
 import datetime
 import paramiko
-from time import sleep
+import time
 
 # Make sure ssh is anabled on VM and ssh key of this machine is in .ssh/authorized_keys of VM.  Username is scheduler
-# Make sure venv is installed 
+
+# Make sure venv is created on VM
 # Necessary: pip install apscheduler torch lightning eco2ai
 # For this example also:  pip install torchvision
 
@@ -24,10 +25,8 @@ def setup_ssh_execution(
 
     vm_main_folder = "/".join(vm_main_path.split("/")[:-1])
     vm_main_file = vm_main_path.split("/")[-1]
-    
-    command = (
-        f"cd {vm_main_folder}; {python_path} {vm_main_file}"
-    )
+
+    command = f"cd {vm_main_folder}; {python_path} {vm_main_file}"
     command += " " + command_arguments
 
     transport = ssh_client.get_transport()
@@ -66,43 +65,49 @@ training_intervals = [
         [
             (
                 datetime.datetime.now(datetime.timezone.utc)
-                + datetime.timedelta(minutes=7, seconds=0),
+                + datetime.timedelta(minutes=8, seconds=0),
                 datetime.datetime.now(datetime.timezone.utc)
-                + datetime.timedelta(minutes=10, seconds=0),
+                + datetime.timedelta(minutes=12, seconds=0),
             )
         ],
     ),
 ]
 
-username = "tiutiulnikov"
-python_path = f"/home/{username}/tiutiulnikov/venv/bin/python"
-vm_main_path = f"/home/{username}/tiutiulnikov/SmartScheduler/scheduler_vm_task/vm_main.py"
-current_ip = "192.168.17.10"
-ssh_port = 44444
-# current_ip = "34.175.199.46"
-# ssh_port = 22
+# username = "tiutiulnikov"
+# python_path = f"/home/{username}/tiutiulnikov/venv/bin/python"
+# vm_main_path = f"/home/{username}/tiutiulnikov/SmartScheduler/scheduler_vm_task/vm_main.py"
+# current_ip = "192.168.17.10"
+# ssh_port = 44444
+
+username = "scheduler"
+python_path = f"/home/{username}/venv/bin/python"
+vm_main_path = f"/home/{username}/scheduler_task/vm_main.py"
+current_ip = "34.175.137.247"
+ssh_port = 22
 
 
 # current_zone = "us-west1-b"
 current_zone = "europe-southwest1-a"
-current_instance_name = "vm"
+current_instance_name = "instance-1"
 project_id = "test-smart-scheduler"
 load_states = False
 
 for zone, time_intervals in training_intervals:
     if zone != current_zone:
-        # new_instance = google_cloud_move_vm(
-        #     current_zone=current_zone, project_id=project_id, new_zone=zone
-        # )
-        print("Moving")
+        print("Moving VM")
+        start_moving_time = time.time()
+        new_instance = google_cloud_move_vm(
+            current_zone=current_zone, current_instance_name=current_instance_name, project_id=project_id, new_zone=zone
+        )
+        print(f"Moving completed in {int(time.time()-start_moving_time)} seconds")
         load_states = True
-        # current_ip = new_instance.network_interfaces[0].access_configs[0].nat_i_p
+        current_ip = new_instance.network_interfaces[0].access_configs[0].nat_i_p
 
-    # Scheduling job 10 seconds before first start_time
-    while datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
-        minutes=3
-    ) < time_intervals[0][0] - datetime.timedelta(seconds=30):
-        sleep(1)
+    # Scheduling job 30 seconds before first start_time
+    while datetime.datetime.now(datetime.timezone.utc) < time_intervals[0][
+        0
+    ] - datetime.timedelta(seconds=30):
+        time.sleep(1)
 
     argument_intervals = [
         (s.strftime("%Y%m%d%H%M%S"), e.strftime("%Y%m%d%H%M%S"))
@@ -127,7 +132,7 @@ for zone, time_intervals in training_intervals:
     try:
         for line in iter(stdout.readline, ""):
             print(line, end="")
-            if 'Traceback' in line:
+            if "Traceback" in line:
                 pass
 
     except KeyboardInterrupt:
